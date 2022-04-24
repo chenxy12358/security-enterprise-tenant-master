693fce3f3c3facaced2582b0bcbe63331ce47f04
chenxy12358 <60055829+chenxy12358@users.noreply.github.com>
2022-04-24 17:01:17 +0800

    按天生成日志

--- renren-admin/src/main/java/io/renren/modules/stationTrack/service/impl/KxStationTrackServiceImpl.java
+++ renren-admin/src/main/java/io/renren/modules/stationTrack/service/impl/KxStationTrackServiceImpl.java
@@ -12,6 +12,8 @@ import io.renren.modules.stationTrack.dao.KxStationTrackDao;
 import io.renren.modules.stationTrack.dto.KxStationTrackDTO;
 import io.renren.modules.stationTrack.entity.KxStationTrackEntity;
 import io.renren.modules.stationTrack.service.KxStationTrackService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,6 +28,7 @@ import java.util.Map;
 @Service
 public class KxStationTrackServiceImpl extends CrudServiceImpl<KxStationTrackDao, KxStationTrackEntity, KxStationTrackDTO> implements KxStationTrackService {
 
+    protected Logger logger = LoggerFactory.getLogger(getClass());
     @Override
     public QueryWrapper<KxStationTrackEntity> getWrapper(Map<String, Object> params) {
         QueryWrapper<KxStationTrackEntity> wrapper = new QueryWrapper<>();
@@ -44,13 +47,22 @@ public class KxStationTrackServiceImpl extends CrudServiceImpl<KxStationTrackDao
     public Result add(JSONObject msgInfo, KxDeviceDTO kxDeviceDTO) {
         String latitude = msgInfo.get("Latitude").toString();//纬度
         String longitude = msgInfo.get("Longitude").toString();//经度
+        logger.debug(latitude);
+        logger.debug(longitude);
         List<KxStationTrackDTO> list = this.getTrackListInfo(kxDeviceDTO.getStationId());
+        logger.debug(kxDeviceDTO.getStationId().toString());
+        logger.debug(String.valueOf(list.size()));
         if (StringUtil.isNotEmpty(latitude) && StringUtil.isNotEmpty(longitude)) {
             if (null != list && list.size() > 0) {
                 KxStationTrackDTO kxStationTrackDTO = list.get(0);
+                logger.debug(String.valueOf(latitude.equals(kxStationTrackDTO.getLat())));
+                logger.debug(String.valueOf(longitude.equals(kxStationTrackDTO.getLng())));
                 if (!latitude.equals(kxStationTrackDTO.getLat()) || !longitude.equals(kxStationTrackDTO.getLng())) {
+                    logger.debug(String.valueOf(!latitude.equals(kxStationTrackDTO.getLat()) || !longitude.equals(kxStationTrackDTO.getLng())));
                     //  保存轨迹坐标点
                     this.saveInfo(kxDeviceDTO,latitude,longitude);
+                    logger.debug("333");
+
                 }
             }else {
                 //桩点第一次的经纬度保存
